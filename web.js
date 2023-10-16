8651d40b0fc72a4e14fec7f4065ffee20f0d5c31
Junho Lee <lastcom1@gmail.com>
2023-10-16 13:32:04 +0900

    Feat. Express 분할
    plan서브도메인을 node에서 나누는게 아니라 nginx에서 나누도록

--- web.js
+++ web.js
@@ -15,6 +15,7 @@ const app = express();
 const plan = express();
 const domain = "junstudy.com";
 const port = 3000;
+const plan_port = 5000;
 
 //view engine 설정 : ejs 사용
 app.set('view engine', 'ejs');
@@ -24,7 +25,7 @@ plan.set('views', path.join(__dirname, 'views')); //폴더, 폴더경로 지정
 
 
 //가상호스트로 서브도메인도 사용되도록 해준 모습
-app.use(vhost(`plan.${domain}`, plan));
+//app.use(vhost(`plan.${domain}`, plan));
 app.use('/', mainRouter);
 plan.use('/', planRouter);
 
@@ -33,5 +34,8 @@ plan.use('/', planRouter);
 
 // .listen(서버띄울 포트번호, 띄운 후 실행할 코드)
 app.listen(port, function() {
+    console.log(`listening on ${port}`);
+});
+plan.listen(plan_port, function() {
     console.log(`listening on ${port}`);
 });
