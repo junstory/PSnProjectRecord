62f139902c8c2d2bc071472653f5675d15f10c6a
Junstory <lastcom1@gmail.com>
2024-05-04 15:14:44 +0900

    Feat: Img Generator added

--- route/planRoute.js
+++ route/planRoute.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const OpenAI = require("openai");
+const dotenv = require("dotenv");
+dotenv.config();
+const gpt = new OpenAI();
+router.use(express.static("public"));
 
-router.use(express.static('public'));
+router.get("/", (req, res) => {
+  res.render("plan");
+});
 
-router.get("/",(req,res)=>{
-  res.render('plan');
-})
+router.get("/test", async (req, res) => {
+  console.log(req.query);
+  const response = await gpt.images.generate({
+    model: "dall-e-3",
+    prompt: req.query.prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  console.log(response);
+  let image_url = response.data[0].url;
+  console.log(image_url);
+  res.send(image_url);
+});
 
-router.use((req,res)=>{
-  res.render('404');
+router.use((req, res) => {
+  res.render("404");
 });
 
 module.exports = router;
