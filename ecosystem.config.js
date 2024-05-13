a7759cc1e749fdb678019f1f5f29219066b32a35
Junstory <lastcom1@gmail.com>
2024-05-13 21:48:12 +0900

    Fix. Same File Removed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,18 @@
-module.exports = { 
-    apps: [{
-      name    : "PSnProjectRecord",	//자신이 만든 프로젝트 이름
-      script  : "npm",
-      args    : "start"
-    }]
-  }
+module.exports = {
+  apps: [
+    {
+      name: "junstudy",
+      script: "server.register.js",
+      instances: 1,
+      exec_mode: "fork",
+      ignore_watch: ["node_modules", "logs", ".git"],
+      log_date_format: "YYYY-MM-DD HH:mm",
+      output: "logs/pm2/junstudy.ouput.log",
+      error: "logs/pm2/junstudy.error.log",
+      //merge_logs: true,
+      autorestart: true,
+      watch: true,
+      // max_memory_restart: "512M",
+    },
+  ],
+};
